ad15c65cb601d71e08689f787286f9a1e925b35c
Chanter327 <march09yuuto@gmail.com>
2024-08-20 23:19:55 +0900

    implement blog page

--- src/components/PageInfo.tsx
+++ src/components/PageInfo.tsx
@@ -7,21 +7,39 @@ const PageInfo: React.FC = () => {
     const url: string = usePathname();
     const searchParams = useSearchParams();
     const lang = searchParams.get('lang');
+
     const modifyUrl = (url: string): string | null => {
-        // 最初の文字を削除し、2文字目を大文字にする
-        if (url.length >= 2) {
-            url = url.slice(1); // 最初の文字を削除
-            url = url.charAt(0).toUpperCase() + url.slice(1); // 2文字目（現在は1文字目）を大文字にする
-            return url
+        // 最初の '/' を削除
+        let modifiedUrl = url.slice(1);
+        
+        // 2番目の '/' の位置を見つける
+        const secondSlashIndex = modifiedUrl.indexOf('/');
+        
+        if (secondSlashIndex !== -1) {
+            // 2番目の '/' までの部分を取得
+            modifiedUrl = modifiedUrl.slice(0, secondSlashIndex);
         }
+        
+        // 最初の文字を大文字にする
+        if (modifiedUrl.length > 0) {
+            modifiedUrl = modifiedUrl.charAt(0).toUpperCase() + modifiedUrl.slice(1);
+            return modifiedUrl;
+        }
+        
         return null;
     }
+
     const modifiedUrl: string | null = modifyUrl(url);
+
     if (url !== '/' && url.charAt(1) !== '?') {
         return (
-            <div className={styles.pageInfo}><Link href={`/?lang=${lang}`}>Top</Link> - {modifiedUrl && modifiedUrl}</div>
+            <div className={styles.pageInfo}>
+                <Link href={`/?lang=${lang}`}>Top</Link> - {modifiedUrl && modifiedUrl}
+            </div>
         );
     }
+
+    return null;
 }
 
 export default PageInfo;
