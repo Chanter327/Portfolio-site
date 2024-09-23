81a4124a8da6e07aad8c90f15cc56b3d8ec06f85
Chanter327 <march09yuuto@gmail.com>
2024-09-23 11:34:11 -0700

    improve site design

--- src/app/apps/calendar/page.tsx
+++ src/app/apps/calendar/page.tsx
@@ -81,12 +81,17 @@ const Calendar: React.FC = () => {
             
             return () => supabase.channel('calendar').unsubscribe();
     }
+
+    const [status, setStatus] = useState<string | null>(null);
     const fetchData = async () => {
+        setStatus('読み込み中...');
         try {
             const response = await getCalendarData();
             setData(response);
+            setStatus(null);
         } catch (error) {
             console.error(`failed to fetch data: ${error}`);
+            setStatus('データの取得に失敗しました。');
         }
     }
 
@@ -95,51 +100,58 @@ const Calendar: React.FC = () => {
         fetchRealTimeData();
     }, []);
 
-    useEffect(() => {
-        fetchData();
-    }, [data]);
+    // useEffect(() => {
+    //     fetchData();
+    // }, [data]);
 
     return (
-        <>
-            {isRegisterActive && register !== null &&
-                <Register handleRegister={handleRegister} isActive year={register!.year} month={register!.month} day={register!.day} wday={register!.wday} />
-            }
-            <div className={styles.calendar}>
-                <div className={styles.today} onClick={backToThisMonth}>{`${currentYear} / ${currentMonth} / ${today.getDate()} (${wdays[today.getDay()]})`}</div>
-                <div className={styles.calendarInfo}>
-                    <div className={styles.handler} onClick={() => handleMonthChange(-1)}>←</div>
-                    <div className={styles.yearMonth}>
-                        <span>{year}</span> 年 <span>{month}</span> 月
+        status !== null ? (
+            <div className={styles.loading}>{status}</div>
+        ) : (
+            data && (
+                <>
+                {isRegisterActive && register !== null &&
+                    <Register handleRegister={handleRegister} isActive year={register!.year} month={register!.month} day={register!.day} wday={register!.wday} />
+                }
+                <div className={styles.calendar}>
+                    <div className={styles.today} onClick={backToThisMonth}>{`${currentYear} / ${currentMonth} / ${today.getDate()} (${wdays[today.getDay()]})`}</div>
+                    <div className={styles.calendarInfo}>
+                        <div className={styles.handler} onClick={() => handleMonthChange(-1)}>←</div>
+                        <div className={styles.yearMonth}>
+                            <span>{year}</span> 年 <span>{month}</span> 月
+                        </div>
+                        <div className={styles.handler} onClick={() => handleMonthChange(1)}>→</div>
                     </div>
-                    <div className={styles.handler} onClick={() => handleMonthChange(1)}>→</div>
-                </div>
-                <div className={styles.wdays}>{wdays.map((d, i) => <li key={i}>{d}</li>)}</div>
-                <div className={styles.days}>
-                    {days.map((d, i) => (
-                        <Link href={`/apps/calendar/${d.year}-${d.month}-${d.day}`} key={i} className={`${styles.day} ${d.year === currentYear && d.month === currentMonth && d.day === today.getDate() && styles.highlight}`}>
-                            <div className={`${styles.date} ${!(d.isCurrentMonth) && styles.diffMonth}`}><div>{d.day}</div></div>
-                            <div className={styles.events}>
-                                {data?.filter(e => d.year === e.year && d.month === e.month && d.day === e.day).map((e, i) => (
-                                    <div
-                                        key={i}
-                                        className={styles.event} 
-                                    >
-                                        <div>{e.title}</div>
-                                    </div>
-                                ))}
-                            </div>
-                        </Link>
-                    ))}
-                </div>
-                <div className={styles.calendarInfo}>
-                    <div className={styles.handler} onClick={() => handleMonthChange(-1)}>←</div>
-                    <div className={styles.yearMonth}>
-                        <span>{year}</span> 年 <span>{month}</span> 月
+                    <div className={styles.addEvent} onClick={() => handleRegister(true, year, month, today.getDate(), wdays[today.getDay()])}>イベントを追加</div>
+                    <div className={styles.wdays}>{wdays.map((d, i) => <li key={i}>{d}</li>)}</div>
+                    <div className={styles.days}>
+                        {days.map((d, i) => (
+                            <Link href={`/apps/calendar/${d.year}-${d.month}-${d.day}`} key={i} className={`${styles.day} ${d.year === currentYear && d.month === currentMonth && d.day === today.getDate() && styles.highlight}`}>
+                                <div className={`${styles.date} ${!(d.isCurrentMonth) && styles.diffMonth}`}><div>{d.day}</div></div>
+                                <div className={styles.events}>
+                                    {data?.filter(e => d.year === e.year && d.month === e.month && d.day === e.day).map((e, i) => (
+                                        <div
+                                            key={i}
+                                            className={styles.event} 
+                                        >
+                                            <div>{e.title}</div>
+                                        </div>
+                                    ))}
+                                </div>
+                            </Link>
+                        ))}
                     </div>
-                    <div className={styles.handler} onClick={() => handleMonthChange(1)}>→</div>
+                    <div className={styles.calendarInfo}>
+                        <div className={styles.handler} onClick={() => handleMonthChange(-1)}>←</div>
+                        <div className={styles.yearMonth}>
+                            <span>{year}</span> 年 <span>{month}</span> 月
+                        </div>
+                        <div className={styles.handler} onClick={() => handleMonthChange(1)}>→</div>
+                    </div>
                 </div>
-            </div>
-        </>
+                </>
+            )
+        )
     );
 };
 
